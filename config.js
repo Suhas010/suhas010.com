a5bc68ae457299473a66aa1a329426d2eecc4d3c
Suhas More <suhas.more@joshsoftware.com>
2020-04-19 20:03:42 +0530

    intial commit

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: 'Suhas More',
+  manifestName: 'Resume',
+  manifestShortName: 'Landing',
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`,
+  firstName: 'Suhas ',
+  lastName: ' More',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/suhas010',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/suhas-more/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/suhas0101',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/suhas010',
+    },
+  ],
+  email: 'hey@suhas010.com',
+  phone: '+91 968 964 9696',
+  address: 'Bawadhan, Pune India',
+};
---
+++ src/pages/index.js
@@ -0,0 +1,306 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+// import { Link } from 'gatsby';
+import Sidebar from '../components/Sidebar';
+import config from '../../config';
+const IndexPage = () => (
+  <Layout>
+    <Sidebar />
+    <div className="container-fluid p-0">
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="about"
+      >
+        <div className="w-100 custome-center">
+          <h1 className="mb-0"><span className="text-primary"> Hello</span> World,</h1>
+          <h1>I'm</h1>
+          <h1 className="mb-0">
+            {config.firstName} 
+            <span className="text-primary">{config.lastName}</span>
+          </h1>
+          {/* <div className="subheading mb-5">
+            {config.address} · <tel>{config.phone}</tel> ·
+            <a href={`mailto:${config.email}`}>{config.email}</a>
+          </div> */}
+          <hr className="small hr" />
+          <p className="lead mb-5">
+            Front-End Engineer with 3+ years' of experience in crafting products and software for various domains like Media, eCommerce
+            CRM and Helth Care.<br/>
+            I spend most of my free time and weekends on sharpening my skills and learning new skills.
+          
+          </p>
+          <div className="social-icons">
+            {config.socialLinks.map(social => {
+              const { icon, url } = social;
+              return (
+                <a key={url} href={url}>
+                  <i className={`fab ${icon}`}></i>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex justify-content-center"
+        id="experience"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Experience</h2>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">Software Engineer</h3>
+              <div className="subheading mb-3">Josh Software</div>
+              <p>
+                Working as a ReactJS developer on Media domain and building inhouse monetory,
+                performance management tool for one of the biggest TV Network world wide.
+                Responsible for Desiging pixel perfect and reusable React Components.
+                Improved codebase and architecture and played important role in performance optimisation 
+              </p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">December 2018 - Present</span>
+            </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">Web Developer</h3>
+              <div className="subheading mb-3">Coditas Technology</div>
+              <p>
+                Capitalize on low hanging fruit to identify a ballpark value
+                added activity to beta test. Override the digital divide with
+                additional clickthroughs from DevOps. Nanotechnology immersion
+                along the information highway will close the loop on focusing
+                solely on the bottom line.
+              </p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">February 2018 - December 2018</span>
+            </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">Junior Software Engineer</h3>
+              <div className="subheading mb-3">Spring Computing Technology</div>
+              <p>
+                Podcasting operational change management inside of workflows to
+                establish a framework. Taking seamless key performance
+                indicators offline to maximise the long tail. Keeping your eye
+                on the ball while performing a deep dive on the start-up
+                mentality to derive convergence on cross-platform integration.
+              </p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary"> June 2017 - February 2018</span>
+            </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
+            <div className="resume-content">
+              <h3 className="mb-0">Intern</h3>
+              <div className="subheading mb-3">Spring Computing Technology</div>
+              <p>
+                Collaboratively administrate empowered markets via plug-and-play
+                networks. Dynamically procrastinate B2C users after installed
+                base benefits. Dramatically visualize customer directed
+                convergence without revolutionary ROI.
+              </p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">January 2017 - June 2017</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="education"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Education</h2>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">University of Colorado Boulder</h3>
+              <div className="subheading mb-3">Bachelor of Science</div>
+              <div>Computer Science - Web Development Track</div>
+              <p>GPA: 3.23</p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">August 2006 - May 2010</span>
+            </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
+            <div className="resume-content">
+              <h3 className="mb-0">James Buchanan High School</h3>
+              <div className="subheading mb-3">Technology Magnet Program</div>
+              <p>GPA: 3.56</p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">August 2002 - May 2006</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="skills"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Skills</h2>
+
+          <div className="subheading mb-3">
+            Programming Languages &amp; Tools
+          </div>
+          <ul className="list-inline dev-icons">
+            <li className="list-inline-item">
+              <i className="fab fa-html5"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-css3-alt"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-js-square"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-angular"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-react"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-node-js"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-sass"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-less"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-wordpress"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-gulp"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-grunt"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-npm"></i>
+            </li>
+          </ul>
+
+          <div className="subheading mb-3">Workflow</div>
+          <ul className="fa-ul mb-0">
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Mobile-First, Responsive Design
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Cross Browser Testing &amp; Debugging
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Cross Functional Teams
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Agile Development &amp; Scrum
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="interests"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Interests</h2>
+          <p>
+            Apart from being a web developer, I enjoy most of my time being
+            outdoors. In the winter, I am an avid skier and novice ice climber.
+            During the warmer months here in Colorado, I enjoy mountain biking,
+            free climbing, and kayaking.
+          </p>
+          <p className="mb-0">
+            When forced indoors, I follow a number of sci-fi and fantasy genre
+            movies and television shows, I am an aspiring chef, and I spend a
+            large amount of my free time exploring the latest technology
+            advancements in the front-end web development world.
+          </p>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="awards"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Awards &amp; Certifications</h2>
+          <ul className="fa-ul mb-0">
+            <li>
+              <i className="fa-li fa fa-certificate  text-warning"></i>
+              Freecodecamp -
+              <a
+                href="https://www.freecodecamp.org/certification/suhas010/responsive-web-design">
+                  JavaScript Algorithms and Data Structures Cirtification
+              </a>
+            </li>
+            <li>
+              <i className="fa-li fa fa-certificate  text-warning"></i>
+              Freecodecamp - 
+              <a
+                href="https://www.freecodecamp.org/certification/suhas010/responsive-web-design">
+                Responsive Web Desiging
+              </a>
+            </li>
+            <li>
+              <i className="fa-li fa fa-trophy text-warning"></i>2<sup>nd</sup>
+              Position in National Level Project Competition Held at M.J Collage Jalgaon, while attending Post Graduation
+            </li>
+            <li>
+              <i className="fa-li fa fa-trophy text-warning"></i>3<sup>rd</sup>
+              Position in National Level Project Competition Held at M.J Collage Jalgaon, while attending Graduation
+            </li>
+            <li>
+              <i className="fa-li fa fa-trophy text-warning"></i>2<sup>nd</sup>
+              Price in Blind Faith Programming Competition
+            </li>
+            <li>
+              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
+              Place - James Buchanan High School - Hackathon 2006
+            </li>
+            <li>
+              <i className="fa-li fa fa-trophy text-warning"></i>3<sup>rd</sup>
+              Place - James Buchanan High School - Hackathon 2005
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
