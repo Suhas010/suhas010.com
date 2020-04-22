5e6b878ebd772d4947dcca603b65ddfc3a5071e9
Suhas More <suhas.more@joshsoftware.com>
2020-04-22 21:13:13 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a8f3c7e20fd759a2d02d.js"
+    "url": "webpack-runtime-36f4a6ed448df2b7ec95.js"
   },
   {
     "url": "framework-37b45856124a722f0eb5.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0e2363c40d26e633948758033384a0a9"
+    "revision": "64b20bdb8602cf72fe329891f6a8ec6c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "ab7b6ce1ca880ef2ef9f940a1035d13b"
+    "revision": "66e45cc8b84a110422e02e5845e410ab"
   },
   {
     "url": "manifest.webmanifest",
