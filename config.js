0cc0144374daafd692e71b55a461ff3610fd79bb
Suhas More <suhas.more@joshsoftware.com>
2020-04-23 22:54:50 +0530

    chnaged name

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/favicon.png',
   pathPrefix: `/`,
-  firstName: 'Suhas ',
+  firstName: 'Suhas s',
   lastName: ' More',
   // social
   socialLinks: [
