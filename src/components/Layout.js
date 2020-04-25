5e4af0c1c6b4f8cf35298218453adda6351e6dd8
Suhas More <suhas.more@joshsoftware.com>
2020-04-25 14:43:16 +0530

    added meta prop for whatsapp

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -31,21 +31,15 @@ class Layout extends Component {
               <html lang="en" />
               <meta name="description" content={data.site.siteMetadata.description} />
               <meta name="image" content={data.site.siteMetadata.image} />
-              {data.site.siteMetadata.url && <meta property="og:url" content={data.site.siteMetadata.url} />}
-              {data.site.siteMetadata.title && <meta property="og:title" content={data.site.siteMetadata.title} />}
-              {data.site.siteMetadata.description && (
-                <meta property="og:description" content={data.site.siteMetadata.description} />
-              )}
-              {data.site.siteMetadata.image && <meta property="og:image" content={data.site.siteMetadata.image} />}
+              <meta property="og:url" content={data.site.siteMetadata.url} />
+              <meta property="og:title" content={data.site.siteMetadata.title} />
+              <meta property="og:description" content={data.site.siteMetadata.description} />
+              <meta property="og:image" content={data.site.siteMetadata.image} />
               <meta name="twitter:card" content="summary_large_image" />
-              {data.site.siteMetadata.twitterUsername && (
-                <meta name="twitter:creator" content={data.site.siteMetadata.twitterUsername} />
-              )}
-              {data.site.siteMetadata.title && <meta name="twitter:title" content={data.site.siteMetadata.title} />}
-              {data.site.siteMetadata.description && (
-                <meta name="twitter:description" content={data.site.siteMetadata.description} />
-              )}
-              {data.site.siteMetadata.image && <meta name="twitter:image" content={data.site.siteMetadata.image} />}
+              <meta name="twitter:creator" content={data.site.siteMetadata.twitterUsername} />
+              <meta name="twitter:title" content={data.site.siteMetadata.title} />
+              <meta name="twitter:description" content={data.site.siteMetadata.description} />
+              <meta name="twitter:image" content={data.site.siteMetadata.image} />
             </Helmet>
             <div className={'main-body'}>{children}</div>
           </>
